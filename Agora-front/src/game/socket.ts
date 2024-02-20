afc9cef6cdef5274b8132c2c6224a61368589347
CGuilhem <93581955+CGuilhem@users.noreply.github.com>
2024-02-20 21:13:44 +0100

    🚧 - SOCKET_TYPES

--- Agora-front/src/game/socket.ts
+++ Agora-front/src/game/socket.ts
@@ -1,1 +1,11 @@
+export const SOCKET_TYPES = {
+  //   JOIN_GAME: 'JOIN_GAME',
+  //   GAME_STATE: 'GAME_STATE',
+  //   MOVE: 'MOVE',
+  //   GAME_OVER: 'GAME_OVER',
+  //   RESET_GAME: 'RESET_GAME',
+  //   LEAVE_GAME: 'LEAVE_GAME',
+  //   ERROR: 'ERROR',
+}
+
 export const socket = new WebSocket('ws://localhost:8080/ws')
